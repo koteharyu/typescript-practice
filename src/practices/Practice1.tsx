fcccc5ef8269be991b59fdc05003460f8fa3ebf7
haryu <67352550+koteharyu@users.noreply.github.com>
2021-09-22 12:05:19 +0900

    引数と返却値の型指定

--- src/practices/Practice1.tsx
+++ src/practices/Practice1.tsx
@@ -1,5 +1,9 @@
 export const Practice1 = () => {
-  const onClickPractice1 = () => alert("practice1");
+  const culcTotalFee = (num: number) => {
+    const total = num * 1.1;
+    console.log(total);
+  };
+  const onClickPractice1 = () => culcTotalFee(1000);
   return (
     <div>
       <p>Practice1: 引数の型指定</p>
